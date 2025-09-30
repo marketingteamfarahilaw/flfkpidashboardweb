69c887eaee16f2e6d3805e4e67ba4c0b577504c9
Raimehn Roger <raimehnroger@gmail.com>
2025-09-30 10:37:45 -0700

    change the connection api

--- assets/js/connection/config.js
+++ assets/js/connection/config.js
@@ -1,7 +1,7 @@
 var origin   = window.location.origin;
 var server = {
-    'CISERVICE'                   : 'https://lmthrp.com/api',
-    'CISITE'                      : 'https://lmthrp.com/web',
+    'CISERVICE'                   : 'http://31.97.43.196/kpidashboardapi',
+    'CISITE'                      : 'http://31.97.43.196/kpidashboard',
 }
 
 var viewRoutes = {
